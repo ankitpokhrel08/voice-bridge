4395812a95adb3323abb2da995e2b905a0b060cf
Aman Dhaubanjar <amandhaubanjar@gmail.com>
2025-05-05 16:13:08 +0545

    auth key removed

--- api method/7_whisper_api.py
+++ api method/7_whisper_api.py
@@ -3,10 +3,10 @@ import requests
 import io
 
 # Replace this with your actual ngrok URL
-API_URL = "https://cfdb-34-173-65-36.ngrok-free.app/transcribe"
+API_URL = "https://880c-34-150-255-94.ngrok-free.app/transcribe"
 
 # Load your full audio file (can be M4A, MP3, WAV, etc.)
-audio = AudioSegment.from_file("../sample audio/my_voice.m4a")
+audio = AudioSegment.from_file("../sample_sound/names.m4a")
 
 
 # Set chunk size (in milliseconds), e.g., 5 seconds
@@ -17,6 +17,8 @@ chunks = [audio[i:i + chunk_duration] for i in range(0, len(audio), chunk_durati
 
 # Loop through each chunk and send it to the API
 for idx, chunk in enumerate(chunks):
+    if idx<2:
+        continue
     print(f"▶️ Sending chunk {idx + 1}/{len(chunks)}")
 
     # Export chunk to memory buffer
