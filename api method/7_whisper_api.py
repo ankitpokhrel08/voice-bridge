e48ab3fd90d2f8007bb279980a3e89221e6e35ae
Aman Dhaubanjar <amandhaubanjar@gmail.com>
2025-05-06 16:20:57 +0545

    language and topic selection added

--- api method/7_whisper_api.py
+++ api method/7_whisper_api.py
@@ -1,13 +1,23 @@
 from pydub import AudioSegment
 import requests
 import io
-
+import pandas as pd 
+from time import time
 # Replace this with your actual ngrok URL
-API_URL = "https://880c-34-150-255-94.ngrok-free.app/transcribe"
+API_URL = "https://427a-35-197-6-142.ngrok-free.app/transcribe"
+
 
 # Load your full audio file (can be M4A, MP3, WAV, etc.)
-audio = AudioSegment.from_file("../sample_sound/names.m4a")
+audio = AudioSegment.from_file("../sample_sound/my_voice.m4a")
 
+lan_list = pd.read_csv("../language.csv")
+
+lan = input("Enter the language: ")
+
+language_code = lan_list[lan_list['Language'] == lan]['Code'].values[0]
+
+print(language_code)
+init_prompt = input("Enter the prompt: ")
 
 # Set chunk size (in milliseconds), e.g., 5 seconds
 chunk_duration = 5000
@@ -15,10 +25,10 @@ chunk_duration = 5000
 # Create audio chunks
 chunks = [audio[i:i + chunk_duration] for i in range(0, len(audio), chunk_duration)]
 
+time1 = time()
 # Loop through each chunk and send it to the API
 for idx, chunk in enumerate(chunks):
-    if idx<2:
-        continue
+
     print(f"▶️ Sending chunk {idx + 1}/{len(chunks)}")
 
     # Export chunk to memory buffer
@@ -30,9 +40,19 @@ for idx, chunk in enumerate(chunks):
     files = {'file': ("chunk.wav", buffer, "audio/wav")}
 
     try:
-        response = requests.post(API_URL, files=files)
+        response = requests.post(
+            API_URL,
+            files=files,
+            data={
+                'lan_code': language_code,
+                'prompt': init_prompt
+            }
+        )
         response.raise_for_status()
         result = response.json().get("translation", "[No output]")
         print(f"✅ Chunk {idx + 1} response: {result}")
     except Exception as e:
         print(f"❌ Error in chunk {idx + 1}: {e}")
+
+time2 = time()
+print(f"Total time taken: {time2 - time1} seconds")
