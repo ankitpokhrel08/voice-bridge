1aaa9139dc0dee45a72d32fb7391d9372e69927b
fuyalasmit <asmitphuyal4040@gmail.com>
2025-05-07 20:16:22 +0545

    display online users - 04

---
+++ components/ListOnlineUsers.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useSocket } from '@/context/SocketContext';
+import { useUser } from '@clerk/nextjs';
+import Avatar from './Avatar';
+
+const ListOnlineUsers = () => {
+  const { user } = useUser();
+  const { onlineUsers } = useSocket();
+  return (
+    <div className="flex border-b border-b-primary/10 w-full items-center pb-2">
+      {onlineUsers &&
+        onlineUsers.map((onlineUser) => {
+          if (onlineUser.userId === user?.id) {
+            return null;
+          }
+
+          return (
+            <div
+              key={onlineUser.userId}
+              className="flex flex-col items-center gap-1 cursor-pointer"
+            >
+              <Avatar src={onlineUser.profile.imageUrl} />
+              <div className="text-sm">
+                {onlineUser.profile.fullName?.split(' ')[0]}
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default ListOnlineUsers;
